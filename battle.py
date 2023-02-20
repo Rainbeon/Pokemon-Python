307266e36d78c9c4603b48b4cab00b40c4fe3f03
Skyler <ev0lvkitten@gmail.com>
2023-02-20 12:44:16 -0500

    Start of battle loop, action select

--- battle.py
+++ battle.py
@@ -16,8 +16,35 @@ def printTeams(team1, team2):
 
     print(team1String + "vs " + team2String)
 
+def printActions(selectedPokemon, team):
+    print("=================")
+    print(f"Actions for {selectedPokemon.name}:")
+    selectedPokemon.printMoves()
+    print("i. Show active pokemon information")
+
+def actionSelect(activePokemon, team):
+    printActions(activePokemon, team)
+    userInput = input()
+
+    return userInput
+
 def battleInit(team1, team2):
     print('Battle start!')
     printTeams(team1, team2)
 
+    battleActive = True
+
+    activePokemonIndex1 = 0
+    activePokemonIndex2 = 0
+
+    print(team1[0].moves)
+
     team1[0].printDetailed()
+    team2[0].printDetailed()
+
+    while(battleActive):
+        userInput1 = actionSelect(team1[activePokemonIndex1], team1)
+        userInput2 = actionSelect(team2[activePokemonIndex2], team2)
+
+        print(userInput1)
+        battleActive = False
